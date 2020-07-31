660d3cf2ee8077841634a3a513972470985799aa
Dmitry <flatorez@ya.ru>
2020-07-31 09:57:39 +0200

    add solutions

--- problems/Climbing Stairs/climbStairs.cs
+++ problems/Climbing Stairs/climbStairs.cs
@@ -1,5 +1,9 @@
 public class Solution {
     public int ClimbStairs(int n) {
+        return climbStairsConstantSpace(n);
+    }
+
+    public int climbStairsConstantSpace(int n) {
         var minusOne = 1;
         var minusTwo = 1;
 
@@ -10,5 +14,18 @@ public class Solution {
         }
 
         return minusOne;
+    }
+
+    public int climbStairsBottomUp(int n) {
+        int[] dp = new int[1 + n];
+
+        Array.Fill(dp, 1);
+
+        for (int idx = 2; n >= idx; ++idx) {
+            dp[idx] = dp[idx - 1] + dp[idx - 2];
+        }
+
+
+        return dp[n];
     }
 }
