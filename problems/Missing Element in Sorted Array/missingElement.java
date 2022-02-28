1e18b3800987f199ec9745cbb7a363c0a3eddd7d
Dmitry Blinov <flatorez@ya.ru>
2022-02-28 22:03:26 +0000

    add solutions

--- problems/Missing Element in Sorted Array/missingElement.java
+++ problems/Missing Element in Sorted Array/missingElement.java
@@ -1,18 +1,28 @@
 class Solution {
     public int missingElement(int[] nums, int k) {
-        int idx = 0;
-        int result = nums[idx];
+        int n = nums.length;
 
-        while (0 < k) {
-            if (nums.length > idx && nums[idx] == result) {
-                ++idx;
-            } else {
-                --k;
-            }
-
-            ++result;
+        if (k > missing(nums, n - 1)) {
+            return nums[n - 1] + k - missing(nums, n - 1);
         }
 
-        return result - 1;
+        int left = 0;
+        int right = n;
+
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+
+            if (k > missing(nums, mid)) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left] + k - missing(nums, left);
+    }
+
+    int missing(int[] nums, int idx) {
+        return nums[idx] - nums[0] - idx;
     }
 }
