926a6a952fcde20a4a0d8aeff8c2b97f3caf8c10
Dmitry Blinov <flatorez@ya.ru>
2022-03-30 07:56:11 +0100

    add solutions

--- problems/Search a 2D Matrix/searchMatrix.cs
+++ problems/Search a 2D Matrix/searchMatrix.cs
@@ -1,21 +1,28 @@
 public class Solution {
     public bool SearchMatrix(int[][] matrix, int target) {
+        if (0 == matrix.Length) {
+            return false;
+        }
+
         int m = matrix.Length;
-        if (m == 0) return false;
         int n = matrix[0].Length;
 
-        // binary search
         var left = 0;
         var right = m * n - 1;
 
         while (left <= right) {
-          var pivotIdx = (left + right) / 2;
-          var pivotElement = matrix[pivotIdx / n][pivotIdx % n];
-          if (target == pivotElement) return true;
-          else {
-            if (target < pivotElement) right = pivotIdx - 1;
-            else left = pivotIdx + 1;
-          }
+            var pivotIdx = (left + right) / 2;
+            var pivotElement = matrix[pivotIdx / n][pivotIdx % n];
+
+            if (target == pivotElement) {
+                return true;
+            } else {
+                if (target < pivotElement) {
+                    right = pivotIdx - 1;
+                } else {
+                    left = pivotIdx + 1;
+                }
+            }
         }
         return false;
     }
