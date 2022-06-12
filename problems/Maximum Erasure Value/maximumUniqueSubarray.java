cd180f2219d9f555df464bb6148f572324a5c760
Dmitry Blinov <flatorez@ya.ru>
2022-06-12 18:12:44 +0100

    add solutions

---
+++ problems/Maximum Erasure Value/maximumUniqueSubarray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        int result = 0;
+        int currentSum = 0;
+        HashSet<Integer> set = new HashSet<>();
+        int start = 0;
+        for (int end = 0; end < nums.length; end++) {
+            // increment start until subarray has unique elements
+            while (set.contains(nums[end])) {
+                set.remove(nums[start]);
+                currentSum -= nums[start];
+                start++;
+            }
+            currentSum += nums[end];
+            set.add(nums[end]);
+            // update result with maximum sum found so far
+            result = Math.max(result, currentSum);
+        }
+        return result;
+    }
+}
