02830a9575fee455f64f88c70a0f8dfed3f78ac4
Dmitry Blinov <flatorez@ya.ru>
2022-06-14 09:43:46 +0100

    add solutions

--- problems/Analyze User Website Visit Pattern/mostVisitedPattern.java
+++ problems/Analyze User Website Visit Pattern/mostVisitedPattern.java
@@ -1,16 +1,17 @@
 class Solution {
     public List<String> mostVisitedPattern(String[] username, int[] timestamp, String[] website) {
-        HashMap<String, Integer> patternFrequencies = new HashMap<>();
+        int n = website.length;
+        String[][] logs = new String[n][3];
         HashMap<String, List<String>> userToPages = new HashMap<>();
-        List<String[]> logs = new ArrayList<>();
-        int maxFrequency = 0;
+        HashMap<String, Integer> store = new HashMap<>();
         String bestPattern = "";
+        int maxCount = 0;
 
-        for (int idx = 0; username.length > idx; ++idx) {
-            logs.add(new String[] {username[idx],String.valueOf(timestamp[idx]),website[idx]});
+        for (int idx = 0; n > idx; ++idx) {
+            logs[idx] = new String[] {username[idx],String.valueOf(timestamp[idx]),website[idx]};
         }
 
-        Collections.sort(logs, (l1, l2) -> Integer.valueOf(l1[1]) - Integer.valueOf(l2[1]));
+        Arrays.sort(logs, (l1,l2)->Integer.valueOf(l1[1])-Integer.valueOf(l2[1]));
 
         for (String[] log : logs) {
             if (!userToPages.containsKey(log[0])) {
@@ -20,37 +21,37 @@ class Solution {
         }
 
         for (String user : userToPages.keySet()) {
-            if (3 > userToPages.get(user).size()) {
-                continue;
-            }
-
             HashSet<String> patterns = getAllPatterns(userToPages.get(user));
 
             for (String pattern : patterns) {
-                int frequency = 1 + patternFrequencies.getOrDefault(pattern, 0);
+                int count = 1 + store.getOrDefault(pattern, 0);
 
-                patternFrequencies.put(pattern, frequency);
-
-                if (frequency > maxFrequency) {
+                if (maxCount < count) {
+                    maxCount = count;
                     bestPattern = pattern;
-                    maxFrequency = frequency;
-                } else if (frequency == maxFrequency) {
+                } else if (maxCount == count) {
                     bestPattern = 0 > pattern.compareTo(bestPattern) ? pattern : bestPattern;
                 }
+
+                store.put(pattern, count);
             }
         }
 
         return Arrays.asList(bestPattern.split(","));
     }
 
-    private HashSet<String> getAllPatterns(List<String> list) {
+    HashSet<String> getAllPatterns(List<String> pages) {
+        int n = pages.size();
         HashSet<String> result = new HashSet<>();
-        int n = list.size();
+
+        if (3 > n) {
+            return result;
+        }
 
         for (int i = 0; n - 2 > i; ++i) {
             for (int j = 1 + i; n - 1 > j; ++j) {
                 for (int k = 1 + j; n > k; ++k) {
-                    result.add(list.get(i)+','+list.get(j)+','+list.get(k));
+                    result.add(pages.get(i)+","+pages.get(j)+","+pages.get(k));
                 }
             }
         }
