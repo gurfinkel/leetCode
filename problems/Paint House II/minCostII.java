42ae90a1da06e4a7515bebc0a76c929a8e4fab2b
Dmitry Blinov <flatorez@gmail.com>
2023-06-03 07:28:55 +0100

    add solutions

--- problems/Paint House II/minCostII.java
+++ problems/Paint House II/minCostII.java
@@ -1,4 +1,28 @@
 class Solution {
+    public int minCostII(int[][] costs) {
+        if (costs.length == 0) return 0;
+        int k = costs[0].length;
+        int n = costs.length;
+
+        for (int house = 1; house < n; house++) {
+            for (int color = 0; color < k; color++) {
+                int min = Integer.MAX_VALUE;
+                for (int previousColor = 0; previousColor < k; previousColor++) {
+                    if (color == previousColor) continue;
+                    min = Math.min(min, costs[house - 1][previousColor]);
+                }
+                costs[house][color] += min;
+            }
+        }
+
+        // Find the minimum in the last row.
+        int min = Integer.MAX_VALUE;
+        for (int c : costs[n - 1]) {
+            min = Math.min(min, c);
+        }
+        return min;
+    }
+
     public boolean validTree(int n, int[][] edges) {
         return validTreeDsu(n, edges);
         // return validTreeDfs(n, edges);
